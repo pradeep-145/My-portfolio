78839b55491410247ad5ef68c741b3f54280a891
pradeep sakthi 👋 <117750940+pradeep-145@users.noreply.github.com>
2024-08-03 17:05:41 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
     }
     return 'Light';
   };
-
   useEffect(() => {
     const systemTheme = getSystemTheme();
     setMainTheme(systemTheme === 'Dark' ? 'dark-main' : 'main');
